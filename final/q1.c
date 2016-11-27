06d6697fd5568cce75d762953ab7d4e8f118c909
William Walter <william.walter@gmail.com>
2016-11-27 15:11:12 -0500

    q1 clean up, solution1

--- final/q1.c
+++ final/q1.c
@@ -5,16 +5,19 @@ int main()
 {
   char s1[80],s2[40];
   char *ans;
+  printf("Input 0 to quit.\n");
   while(1){
-  printf("Please input a string of less than 80 characters, followed by a new line.\n>");
+  printf("Please input a string characters, followed by a new line.\n>");
   fgets(&s1[0],81,stdin);
-  printf("Please input a string of less than 40 characters, followed by a new line.\n>");
-  fgets(&s2[0],81,stdin);
   if(s1[0] == '0')
     break;
-  printf("string1: %s\nstring2: %s\n", s1, s2);
+  printf("Please input a string of less than 39 characters, followed by a new line.\n>");
+  fgets(&s2[0],41,stdin);
+  if(s2[0] == '0')
+    break;
+  printf("string1: %sstring2: %sinput size: %d\n", s1, s2, string_size(s1) + string_size(s2));
   ans = string_cat(s1,s2);
-  printf("ans: %s", ans);
+  printf("ans: %s\n", ans);
   }
 }
 char* string_cat(char * s1, char * s2)
@@ -23,26 +26,25 @@ char* string_cat(char * s1, char * s2)
   char * start = s1;
   char * string2 = s2;
   size = string_size(start) + string_size(string2);
-  printf("size: %d\nstring: %s\n", size, start);
   if( size > 80 )
     return NULL;
 
-  while(*start)
+  while(*start != '\n')
   {
     //find the address of the end of string 1
     ++start;
     ++counter;
   }
-  while(*string2)
+  while(*string2 != '\n')
   {
+    
     *start = *string2++;
     ++start;
     ++counter;
   }
-  printf("counter: %d\nstring: %s\n", counter, start);
-  ++start;
-   start = 0;
-  printf("size: %d\n", counter);
+  *start = '\0';
+  // This will be max 78 cause of the stripped new line chars
+  printf("output size: %d\n", counter);
   return s1;
 }
 int string_size(char*s)
